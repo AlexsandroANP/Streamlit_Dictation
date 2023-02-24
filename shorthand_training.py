6b10a3d76b597657ae8e627cf2ce20cd36e22d3a
AlexsandroANP <126203856+AlexsandroANP@users.noreply.github.com>
2023-02-24 17:25:18 +0800

    Update shorthand_training.py

--- shorthand_training.py
+++ shorthand_training.py
@@ -9,8 +9,14 @@ import time #计时库
 import datetime
 import re
 
+# 设置网页标题
+st.title('英语听力速记训练')
+st.markdown("__*可以随机生成一段用于英语听力速记的音频*__")
 
-
+for curDir, dirs, files in os.walk("/app/Streamlit_Dictation/", topdown=True):
+  st.text("现在的目录：" + curDir)
+  st.text("该目录下包含的子目录：" + str(dirs))
+  st.text("该目录下包含的文件：" + str(files))
 
 a1 = AudioSegment.from_wav('/app/Streamlit_Dictation/audio_chuncks/a.wav')
 a2 = AudioSegment.from_wav('/app/Streamlit_Dictation/audio_chuncks/b.wav')
@@ -26,9 +32,7 @@ final_track = a1+a2
 buff = final_track.raw_data
 #print(buff)
 #print(type(buff))
-# 设置网页标题
-st.title('英语听力速记训练')
-st.markdown("__*可以随机生成一段用于英语听力速记的音频*__")
+
 st.audio(buff)
 
 #samples = final_track.get_array_of_samples()
