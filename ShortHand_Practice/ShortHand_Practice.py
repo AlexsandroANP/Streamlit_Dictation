18f96264a55cf013eeece20207c38f2626798c40
AlexsandroANP <126203856+AlexsandroANP@users.noreply.github.com>
2023-02-26 16:43:16 +0800

    Update ShortHand_Practice.py

--- ShortHand_Practice/ShortHand_Practice.py
+++ ShortHand_Practice/ShortHand_Practice.py
@@ -31,13 +31,15 @@ def vocaburaries_displayer(playlist):
             st.text('{:0>2d} {:^20}'.format(playlist.index(i)+1,name[0]))
             
 
+f_view = os.listdir()
+for i in f_view:
+    st.text(f_view)
+        
 with st.spinner('Fetching Chuncks ...'):
     chuncks_list = glob.glob('/app/streamlit_apps/shorthand_practice/audio_chuncks/*.ogg')
     chuncks_numbers = len(chuncks_list)
     
-    f_view = os.listdir()
-    for i in f_view:
-        st.text(f_view)
+
     
 st.title('ShortHand Practice for Dictation')
 st.caption('英文关键词速记练习')
